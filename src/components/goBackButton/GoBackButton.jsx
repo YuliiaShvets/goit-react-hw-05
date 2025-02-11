93d1f2a6289d6eaba6b2a34edccdbc1fde60297d
Yuliia Shvets <y.shvets85@gmail.com>
2025-02-11 22:50:45 +0100

    add .

--- src/components/goBackButton/GoBackButton.jsx
+++ src/components/goBackButton/GoBackButton.jsx
@@ -3,23 +3,22 @@ import { useRef } from "react";
 import s from "../goBackButton/GoBackButton.module.css";
 
 const GoBackButton = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-
-
-    const backLink = useRef(
-        location.state?.from === "/" ? "/" : location.state?.from ?? "/movies"
-    );
+        const location = useLocation();
+        const navigate = useNavigate();
     
-    const handleGoBack = () => {
-        navigate(backLink.current);
-    };
-
-    return (
-        <button className={s.goBackButton} onClick={handleGoBack}>
-            Go Back
-        </button>
-    );
-}
-
-export default GoBackButton;
+    
+        const backLink = useRef(location.state ?.from ?? "/movies");
+    
+    
+        const handleGoBack = () => {
+            navigate(backLink.current);
+        };
+    
+        return (
+            <button className={s.backButton} onClick={handleGoBack}>
+                Go Back
+            </button>
+        );
+    }
+    
+    export default GoBackButton;
--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,27 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import s from "./MovieList.module.css";
 
-const MovieList = ({ movies, isLoading }) => {
+const  MovieList = ({ movies, isLoading }) => {
+    const location = useLocation();
     if (isLoading) {
         return <p>Loading...</p>;
     }
-
     return (
-        <ul className={s.movieList}>
-            {movies.map(movie => (
-                <li key={movie.id} className={s.movieItem}>
-                    <Link to={`/movies/${movie.id}`}>
-                        <img
-                            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                            alt={movie.title}
-                            className={s.movieImage}
-                        />
-                        <p className={s.movieTitle}>{movie.title}</p>
-                    </Link>
-                </li>
-            ))}
-        </ul>
+        <>
+            {movies.length > 0 ? (
+                <ul className={s.listMovies}>
+                    {movies.map(({ id, title, poster_path }) => (
+                        <li key={id} className={s.movieItem}>
+                            <Link
+                                className={s.link}
+                                to={`/movies/${id}`}
+                                state={{ from: location }}
+                            >
+                                <img
+                                    className={s.poster}
+                                    src={poster_path ? `https://image.tmdb.org/t/p/w200${poster_path}` : "https://via.placeholder.com/200x300"}
+                                    alt={title}
+                                />
+                                <p>{title}</p>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No movies</p>
+            )}
+        </>
     );
-};
+}
 
 export default MovieList;
